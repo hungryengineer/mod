a1da417ffff925d4a70faba625429f8cde1a10d4
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-21 01:15:21 +0530

    tuple as vpcid

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private_subnets" {
  count             = length(var.private_subnet_cidrs)
- vpc_id            = var.vpc_id
+ vpc_id            = var.vpc_id[*]
  cidr_block        = element(var.private_subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index) # new availability zone per subnet
  tags              = var.tags #"Private Subnet ${count.index + 1}"
