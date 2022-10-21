466d0d0e2fae259d6c621141074ec516d2885099
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-21 13:03:58 +0530

    nacl

--- nacl/outputs.tf
+++ nacl/outputs.tf
@@ -1,7 +1,7 @@
-# output "nacl_id" {
-#     value = aws_network_acl.main[*].id
-# }
+output "nacl_id" {
+    value = aws_network_acl.main[*].id
+}
 
-# output "nacl_arn" {
-#     value = aws_network_acl.main[*].arn
-# }
+output "nacl_arn" {
+    value = aws_network_acl.main[*].arn
+}
