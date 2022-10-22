37df4528514aa05958d5f1e83988a9dbf59aae42
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-22 18:39:47 +0530

    added subnet names

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,6 +1,5 @@
 resource "aws_subnet" "subnets" {
  count             = length(var.subnet_cidrs)
- name              = var.name
  vpc_id            = var.vpc_id
  cidr_block        = element(var.subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index) # new availability zone per subnet
