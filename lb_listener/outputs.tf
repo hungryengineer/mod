8919cf1cd01e119bbb02923f0eaae436e060b8a7
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-24 18:27:14 +0530

    lb_listener

--- lb_listener/outputs.tf
+++ lb_listener/outputs.tf
@@ -1,7 +1,7 @@
 output "lb_listener_id" {
-    value = aws_lb_listener[*].frontend_http_tcp.id
+    value = aws_lb_listener.frontend_http_tcp[*].id
 }
 
 output "lb_listener_arn" {
-    value = aws_lb_listener[*].frontend_http_tcp.arn
+    value = aws_lb_listener.frontend_http_tcp[*].arn
 }
