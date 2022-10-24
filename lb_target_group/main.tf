ee71bf264362e1304bdff9802607ae55293a3398
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-24 07:05:48 +0530

    lb target group

---
+++ lb_target_group/main.tf
@@ -0,0 +1,36 @@
+resource "aws_lb_target_group" "main" {
+  count = var.create_target_group ? 1 : 0
+  name        = var.name
+  port        = var.port
+  protocol    = var.protocol
+  target_type = var.target_type
+  vpc_id      = var.vpc_id
+  load_balancing_algorithm_type = var.load_balancing_algorithm_type
+  ip_address_type = var.ip_address_type
+  
+  dynamic health_check {
+    for_each = var.health_check != null ? [var.health_check] : []
+     content {
+      enabled             = try(health_check.value.enabled, null)
+      interval            = try(health_check.value.interval, null)
+      path                = try(health_check.value.path, null)
+      port                = try(health_check.value.port, null)
+      healthy_threshold   = try(health_check.value.healthy_threshold, null)
+      unhealthy_threshold = try(health_check.value.unhealthy_threshold, null)
+      timeout             = try(health_check.value.timeout, null)
+      protocol            = try(health_check.value.protocol, null)
+      matcher             = try(health_check.value.matcher, null)
+    }
+  }
+
+  dynamic stickiness {
+    for_each = var.stickiness != null ? [var.stickiness] : []
+    content {
+     enabled         = try(stickiness.value.enabled, true)
+     type            = stickiness.value.type
+     cookie_name     = try(stickiness.value.cookie_name, null)
+     cookie_duration = try(stickiness.value.cookie_duration, null)
+    }
+  }
+}
+
---
+++ lb_target_group/vars.tf
@@ -0,0 +1,39 @@
+variable "create_target_group" {
+  
+}
+
+variable "name" {
+  
+}
+
+variable "port" {
+  
+}
+
+variable "protocol" {
+  
+}
+
+variable "target_type" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "load_balancing_algorithm_type" {
+  
+}
+
+variable "ip_address_type" {
+  
+}
+
+variable "health_check" {
+  
+}
+
+variable "stickiness" {
+  
+}
