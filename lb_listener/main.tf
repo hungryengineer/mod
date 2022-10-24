9d47896aef421d180e2c2df2810914f9868dada6
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-24 17:33:45 +0530

    lb_listener

---
+++ lb_listener/main.tf
@@ -0,0 +1,41 @@
+resource "aws_lb_listener" "frontend_http_tcp" {
+  count = var.create_lb_listener ? 1 : 0
+  load_balancer_arn = var.load_balancer_arn
+  port     = var.port
+  protocol = var.protocol
+
+  dynamic "default_action" {
+    for_each = var.default_action != null ? [var.default_action] : []
+
+    # Defaults to forward action if action_type not specified
+    content {
+      type             = try(default_action.value.action_type, "forward")
+      target_group_arn = var.target_group_arn
+
+    #   dynamic "redirect" {
+    #     for_each = var.redirect != null ? [var.redirect] : []
+
+    #     content {
+    #       path        = try(redirect.value.path, null)
+    #       host        = try(redirect.value.host, null)
+    #       port        = try(redirect.value.port, null)
+    #       protocol    = try(redirect.value.protocol, null)
+    #       query       = try(redirect.value.query, null)
+    #       status_code = try(redirect.value["status_code"])
+    #     }
+    #   }
+
+      dynamic "fixed_response" {
+        for_each = var.fixed_response != null ? [var.fixed_response] : []
+
+        content {
+          content_type = fixed_response.value["content_type"]
+          message_body = try(fixed_response.value.message_body, null)
+          status_code  = fixed_response.value["status_code"]
+        }
+      }
+    }
+  }
+
+  tags =var.tags
+}
---
+++ lb_listener/vars.tf
@@ -0,0 +1,31 @@
+variable "create_lb_listener" {
+  
+}
+
+variable "load_balancer_arn" {
+  
+}
+
+variable "port" {
+  
+}
+
+variable "protocol" {
+  
+}
+
+variable "default_action" {
+  
+}
+
+variable "target_group_arn" {
+  
+}
+
+variable "fixed_response" {
+  
+}
+
+variable "tags" {
+  
+}
