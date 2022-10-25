73e472ef8d76256e8a5cc5548347e2b9c684603b
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-25 20:34:07 +0530

    lb_listener_rule

--- lb_listener_rule/outputs.tf
+++ lb_listener_rule/outputs.tf
@@ -1,7 +1,7 @@
-output "lb_listener_rule.id" {
+output "lb_listener_rule_id" {
     value = aws_lb_listener_rule.host_based_routing[*].id
 }
 
-output "lb_listener_rule.arn" {
+output "lb_listener_rule_arn" {
     value = aws_lb_listener_rule.host_based_routing[*].arn
 }
