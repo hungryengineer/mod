e8033adf28b7475b5dec47a6b643c4afb32ab63d
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-26 13:01:00 +0530

    added the id fro ingress rule

--- security_group/outputs.tf
+++ security_group/outputs.tf
@@ -5,3 +5,7 @@ output "security_group_id" {
 output "security_group_arn" {
     value = aws_security_group.main[*].arn
 }
+
+output "security_group_ingress_id" {
+    value = aws_security_group.main.ingress[*].id
+}
