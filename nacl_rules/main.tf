eeeddd368d66a34e0ce739017b3822ef7594f017
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-26 14:22:35 +0530

    trials with SG rules

--- nacl_rules/main.tf
+++ nacl_rules/main.tf
@@ -6,6 +6,6 @@ resource "aws_network_acl_rule" "main" {
   protocol       = element(var.protocol, count.index)
   rule_action    = var.rule_action
   cidr_block     = element(var.cidr_block, count.index)
-  from_port      = var.from_port
-  to_port        = var.to_port
+  from_port      = element(var.from_port, count.index)
+  to_port        = element(var.to_port, count.index)
 }
