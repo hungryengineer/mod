680d7918982aee5ba5aa13138e9fc71148542647
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-31 13:10:03 +0530

    route_53-record

--- route53_record/main.tf
+++ route53_record/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "main" {
-  count   = length(var.records) 
+  count   = var.create_records ? 1 : 0 
   zone_id = var.zone_id
   name    = var.name
   type    = var.type
