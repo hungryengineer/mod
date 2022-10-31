1b0b9c8f6feaf07be7aa1f82327f16b3977c6360
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-10-31 13:03:43 +0530

    route_53-record

--- route53_record/main.tf
+++ route53_record/main.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_record" "main" {
   count   = length(var.records) 
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = 300
+  zone_id = var.zone_id
+  name    = var.name
+  type    = var.type
+  ttl     = var.ttl
   records = element(var.records, count.index)
   dynamic "weighted_routing_policy" {
     for_each = length(var.weighted_routing_policy) == 0 ? [] : [true]
--- route53_record/vars.tf
+++ route53_record/vars.tf
@@ -2,6 +2,22 @@ variable "records" {
   
 }
 
+variable "zone_id" {
+  
+}
+
+variable "name" {
+  
+}
+
+variable "type" {
+  
+}
+
+variable "ttl" {
+  
+}
+
 variable "weighted_routing_policy" {
   
 }
