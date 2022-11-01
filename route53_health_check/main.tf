3848fb63568a5b4ce0fd7c491444131f6f74bfb4
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-11-01 22:23:52 +0530

    route_53_health_check

--- route53_health_check/main.tf
+++ route53_health_check/main.tf
@@ -2,7 +2,7 @@ resource "aws_route53_health_check" "primary" {
   count             = var.create_health_check ? 1 : 0
   fqdn              = var.fqdn
   port              = var.port
-  type              = var.type
+  type              = element(var.type, count.index)
   resource_path     = var.resource_path
   failure_threshold = var.failure_threshold
   request_interval  = var.request_interval
