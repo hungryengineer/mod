0b73e39e8f3188da14bbe222d1bb1c7a54cf059f
Kumar Anshuman <kumar.anshuman@nagarro.com>
2022-11-04 11:24:08 +0530

    ecr repo

--- ecr_repository/outputs.tf
+++ ecr_repository/outputs.tf
@@ -1,11 +1,11 @@
 output "ecr_repository_arn" {
-    value = aws_ecr_repository.main[*].ecr_repository_arn
+    value = aws_ecr_repository.main[*].arn
 }
 
 output "ecr_repository_registry_id" {
-    value = aws_ecr_repository.main[*].ecr_repository_registry_id
+    value = aws_ecr_repository.main[*].registry_id
 }
 
 output "ecr_repository_repository_url" {
-    value = aws_ecr_repository.main[*].ecr_repository_repository_url
+    value = aws_ecr_repository.main[*].repository_url
 }
